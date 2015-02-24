ffd93ecb9aaa894ce9366944a492253f97a45a1f
Blake Embrey <hello@blakeembrey.com>
2015-02-24 18:04:25 +0700

    tidy up vendor support and replace social buttons

--- config/define.js
+++ config/define.js
@@ -5,10 +5,7 @@ module.exports = {
     description: 'Avid software engineer who occasionally finds time to compose a few words.'
   },
   integrations: {
-    twitter: 'blakeembrey',
     disqus: 'blakeembrey',
-    facebook: true,
-    coinbase: '528e3adad0b02fe128000548',
     analytics: 'UA-22855713-2'
   },
   owner: {
