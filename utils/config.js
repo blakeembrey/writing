533f6305cb5119624abb2da2663fa0e44b98f733
Blake Embrey <hello@blakeembrey.com>
2017-04-09 14:37:37 -0700

    Update site description

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 exports.config = {
   siteName: 'Blake Embrey',
-  siteDescription: 'Just some words.',
+  siteDescription: 'A personally curated collection of words hosted in cyberspace for an indeterminate duration.',
   siteRepo: 'https://github.com/blakeembrey/knowledge',
   siteUrl: 'http://blakeembrey.com',
   twitter: 'https://twitter.com/blakeembrey',
