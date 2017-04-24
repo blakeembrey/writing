0cc76be34e2d77459ec964d3a7ba92d435a8ee66
Blake Embrey <hello@blakeembrey.com>
2017-04-24 12:15:40 -0700

    Move repo to `blakeembrey/writing`
    It makes a lot more sense than being domain specific or "knowledge" since I'm terrible at trying to document other stuff right now.

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 exports.config = {
   siteName: 'Blake Embrey',
-  siteDescription: 'A personally curated collection of words hosted in cyberspace for an indeterminate duration.',
-  siteRepo: 'https://github.com/blakeembrey/knowledge',
+  siteDescription: 'A personally curated collection of words hosted in cyberspace for some indeterminate duration.',
+  siteRepo: 'https://github.com/blakeembrey/writing',
   siteUrl: 'http://blakeembrey.com',
   twitter: 'https://twitter.com/blakeembrey',
   authorName: 'Blake Embrey',
